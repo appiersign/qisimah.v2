2eba5e4fe6978a28d3f140a3f27fd75fe6ffa009
iamKofi <kofi@qisimah.com>
2018-05-29 23:46:24 +0000

    submit sign up form feature created

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">User Registration</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    <form class="form-horizontal" method="POST" action="{{ url('email.verify') }}">
                         {{ csrf_field() }}
 
                         {{--<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">--}}
