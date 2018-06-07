ec40e645e59f6a85a6fe187abef6148f42ff8cd3
Solomon Appier-Sign <solomon@qisimah.com>
2018-06-07 15:00:19 +0000

    Producer model. migration and resource controller created

---
+++ app/Http/Controllers/ProduceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Produce;
+use Illuminate\Http\Request;
+
+class ProduceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Produce  $produce
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Produce $produce)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Produce  $produce
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Produce $produce)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Produce  $produce
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Produce $produce)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Produce  $produce
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Produce $produce)
+    {
+        //
+    }
+}
