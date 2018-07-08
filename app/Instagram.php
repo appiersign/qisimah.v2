72b9399d1c69cfd49f035c122033ec54507f954c
Solomon Appier-Sign <solomon@qisimah.com>
2018-07-08 03:45:52 +0000

    fillable property of instagram model defined

--- app/Instagram.php
+++ app/Instagram.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Instagram extends Model
 {
-    //
+    protected $fillable = ['qisimah_id', 'external_id', 'user_name', 'full_name', 'avatar', 'bio', 'website', 'is_business', 'media', 'follows', 'followed_by', 'likes', 'comments', 'last_request', 'last_media_request'];
+    
 }
