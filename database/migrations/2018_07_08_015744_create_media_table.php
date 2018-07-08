7850dedcbffedfe10c674fd2be66bf2fff6daf7c
Solomon Appier-Sign <solomon@qisimah.com>
2018-07-08 03:36:06 +0000

    media migration defined

--- database/migrations/2018_07_08_015744_create_media_table.php
+++ database/migrations/2018_07_08_015744_create_media_table.php
@@ -15,6 +15,18 @@ class CreateMediaTable extends Migration
     {
         Schema::create('media', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('qisimah_id')->unique();
+            $table->string('external_id')->unique();
+            $table->string('insta_id')->index();
+            $table->string('type');
+            $table->string('link');
+            $table->boolean('user_has_liked')->default(false);
+            $table->unsignedInteger('likes')->default(0);
+            $table->unsignedInteger('comments')->default(0);
+            $table->text('avatar')->nullable();
+            $table->text('avatar_standard')->nullable();
+            $table->text('avatar_low')->nullable();
+            $table->integer('created_time');
             $table->timestamps();
         });
     }
