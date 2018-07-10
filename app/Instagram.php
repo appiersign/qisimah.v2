40c5b2e16715b20c909c20e9c66311c2a0263cfa
Solomon Appier-Sign <solomon@qisimah.com>
2018-07-10 00:35:32 +0000

    sendRequest method moved into helpers as sendRequest function

--- app/Instagram.php
+++ app/Instagram.php
@@ -3,9 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class Instagram extends Model
 {
     protected $fillable = ['qisimah_id', 'external_id', 'user_name', 'full_name', 'avatar', 'bio', 'website', 'is_business', 'media', 'follows', 'followed_by', 'likes', 'comments', 'last_request', 'last_media_request'];
-    
+
+    private $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
 }
--- app/helpers.php
+++ app/helpers.php
@@ -1,9 +1,29 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
+
 function generateVerificationCode($code = '')
 {
     if ($code){ return $code; }
 
     $string = str_shuffle('0147852036987456321');
     return substr($string, 0, 3). '-'. substr(str_shuffle($string), 0, 3);
+}
+
+function sendRequest($url, $headers = ['Content-Type: application/json'], $fields = '', $method = 'GET')
+{
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    if ($method === 'POST') {
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $fields);
+    }
+    try {
+        return curl_exec($curl);
+    } catch (\Exception $exception) {
+        Log::info('Instagram SendRequest Error');
+        Log::error($exception->getCode() . ' => ' . $exception->getMessage());
+    }
 }
