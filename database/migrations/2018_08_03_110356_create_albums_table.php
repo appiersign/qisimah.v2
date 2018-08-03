ac8d4f713a8111705cc1d4cb21bf6233acf258d7
Solomon Appier-Sign <solomon@qisimah.com>
2018-08-03 11:53:34 +0000

    albums migration written

---
+++ database/migrations/2018_08_03_110356_create_albums_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->index();
+            $table->string('release_year');
+            $table->unsignedInteger('label_id');
+            $table->timestamps();
+
+            $table->foreign('label_id')
+                ->references('id')
+                ->on('labels')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
