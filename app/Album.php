ca336638cd9276432bde5a160bc2eed6e9b6cfcd
Solomon Appier-Sign <solomon@qisimah.com>
2018-08-03 12:10:14 +0000

    label() and songs() relationship defined in Album

--- app/Album.php
+++ app/Album.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use App\Models\Song;
 use Illuminate\Database\Eloquent\Model;
 
 class Album extends Model
 {
-    //
+    public function label()
+    {
+        return $this->belongsTo(Label::class);
+    }
+
+    public function songs()
+    {
+        return $this->belongsTo(Song::class);
+    }
 }
