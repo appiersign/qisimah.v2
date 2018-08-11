16543b34736f03d1af3cbb7cc7a5f68878bab632
Solomon Appier-Sign <solomon@qisimah.com>
2018-08-11 07:35:57 +0000

    Artist Song Pivot
    Migration for artist_song generated to enable many-to-many relationship
    between Artist and Song

---
+++ database/migrations/2018_08_11_073243_create_artist_song_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArtistSongTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artist_song', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artist_song');
+    }
+}
