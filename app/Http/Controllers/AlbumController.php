70e7462375510bd9a18d4625179d49d0c11c380b
Solomon Appier-Sign <solomon@qisimah.com>
2018-08-13 05:52:58 +0000

    Show songs on Album
    show() parameter updated to qisimah_id instead of album id to make it
    difficult for users to guess the next album id.
    show view updated to display songs on Album.

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -64,7 +64,7 @@ class AlbumController extends Controller
      */
     public function show(string $qisimah_id)
     {
-        $album = Album::with('songs')->where('qisimah_id', $qisimah_id)->first();
+        $album = Album::with('songs.featured')->where('qisimah_id', $qisimah_id)->first();
         $artists = [];
         return view('pages.albums.show', compact('artists', 'album'));
     }
