a7ceda50d49103cf3ac0aef50f98798f292aa0af
Solomon Appier-Sign <solomon@qisimah.com>
2018-09-03 14:21:59 +0000

    songs() relationship defined on Play

--- app/Play.php
+++ app/Play.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use App\Artist;
+use App\Song;
 use Illuminate\Database\Eloquent\Model;
 
 class Play extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function songs()
+    {
+        return $this->hasMany(Song::class, 'qisimah_id', 'audio_id');
+    }
+
+    public function artists()
+    {
+        return $this->hasManyThrough(Artist::class, Song::class, 'qisimah_id');
+    }
 }
