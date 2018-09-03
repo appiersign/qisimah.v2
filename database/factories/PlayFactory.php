ee82242d557a41925ad09f9fe1f1b8c89afa417e
Solomon Appier-Sign <solomon@qisimah.com>
2018-09-03 14:30:23 +0000

    PlayFactory updated

--- database/factories/PlayFactory.php
+++ database/factories/PlayFactory.php
@@ -3,7 +3,11 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Play::class, function (Faker $faker) {
+    $song = \App\Song::inRandomOrder()->first();
+
     return [
-        //
+        'stream_id' =>  \App\Broadcaster::inRandomOrder()->first()->stream_id,
+        'audio_id'  =>  $song->qisimah_id,
+        'acr_id'    =>  $song->acr_id
     ];
 });
