44514e11630d9ed73eae3b91214e7fca38a00aa6
iamKofi <kofi@qisimah.com>
2018-09-27 23:30:18 +0000

    update placeholder for email input

--- resources/views/pages/guest/password-reset.blade.php
+++ resources/views/pages/guest/password-reset.blade.php
@@ -6,7 +6,7 @@
                 <form action="{{ url('log.in') }}" method="post" id="qisimah-login-form" name="email-form" class="form">
                     {{ csrf_field() }}
                     <label for="username" class="field-label-2">Email:</label>
-                    <input type="email" class="form-field w-input" name="email" data-name="username" placeholder="Enter your name" id="username" required="">
+                    <input type="email" class="form-field w-input" name="email" data-name="username" placeholder="Enter your email" id="username" required="">
                     <p>Insert your email and we’ll send you a link to the password reset page.</p><br>
                     <input type="submit" value="Retrieve Password" data-wait="Please wait..." class="submit-button w-button">
                     <input type="submit" value="Wait, I Remember" data-wait="Please wait..." class="submit-button w-button">
